7156404eb87d836f49f136ea239b7c9ff73dc053
Marcus Tran <marcus@hoiio.com>
2016-04-28 16:14:14 +0700

    init

--- js/component.js
+++ js/component.js
@@ -1,123 +1,14 @@
 $(document).ready(function() {
     
-    
-    
-    
-    
-    /*--------------------
-        DROPDOWN
-    ----------------------*/ 
-    
-    // Standard
-    $('.ui.dropdown').dropdown();
-    
-    $('.ui.sidebar')
-        .sidebar('setting', 'transition', 'overlay')
-        .sidebar('toggle')
-    ;
-    
-    
-    /*--------------------
-        PROGRESS
-    ----------------------*/ 
-    
-    // Standard
-    var 
-    $progress = $('.ui.progress').not('.success, .error, .warning'),
-        $buttons = $('.example .increment.button, .example .decrement.button'),
-        $stateProgress    = $('.ui.success.progress, .ui.warning.progress, .ui.error.progress');
-    
-    $stateProgress
-      .progress('set progress', 100)
-    ;
-    
-    $progress
-      .each(function() {
-        $(this)
-          .progress({
-            showActivity : false,
-            random       : {
-              min : 5,
-              max : 15
-            },
-            percent      : Math.floor(Math.random() * 60) + 5
-          })
-        ;
-      })
-    ;
-    
-    $buttons.on('click', function() {
-        var $progress = $(this).closest('.example').find('.ui.progress')
-        ;
-        if( $(this).hasClass('increment') ) {
-          $progress.progress('increment');
-        }
-        else {
-          $progress.progress('decrement');
-        }
-      })
-    ;
-    
-    
-    /*--------------------
-        ACCORDION
-    ----------------------*/ 
-    
-    $('.ui.accordion')
-        .accordion()
-    ;
-    
-    
-    /*--------------------
-        MENU
-    ----------------------*/ 
-   
-    /*--------------------
-        MESSAGE
-    ----------------------*/ 
-    
-    // Transition
-    $('.message .remove')
-      .on('click', function() {
-        $(this)
-          .closest('.message')
-          .transition('fade')
-        ;
-      })
-    ;
-    
-    /*--------------------
-        TABLE
-    ----------------------*/ 
-    
-    
-    /*--------------------
-        MODAL
-    ----------------------*/ 
-    
     $('.present.modal')
-        .modal('attach events', '#clearing-row button')
-    ;
+        .modal('attach events', '#clearing-row button');
     
-    /*--------------------
-        Navigation
-    ----------------------*/
-    
-    // Set Active
-    function capitalise(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-    }
-    
-    var x = window.location.href.match(/.*\/(.*)$/)[1];
+    var x =  window.location.hash;
     
     if(x){
-        $('a[href$='+x+']').addClass("active").attr('href', '').css({'cursor': 'pointer', 'pointer-events' : 'none'});
+        $(document).scrollTo('#' + x);
     }
-    else{
-        $('a[href$=intro]').addClass("active").attr('href', '').css({'cursor': 'pointer', 'pointer-events' : 'none'});
-    } 
    
- 
 });
 
 
@@ -126,6 +17,11 @@ $(window).load(function() {
     // Animate loader off screen
     $(".hoiio.loading").fadeOut("slow");
 
+    hljs.initHighlightingOnLoad();
+    
+    
+});
+     
 });
 
 $(document).bind('scroll',function(e){
@@ -142,9 +38,7 @@ $(document).bind('scroll',function(e){
     });
 });
 
-hljs.initHighlightingOnLoad();
 
 $('a.active').click(function(){
     return false;
-});
-          
+     
