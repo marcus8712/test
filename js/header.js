fe3a9f49ba362082e304d54a911955a3e516deb4
Marcus Tran <marcus@hoiio.com>
2016-04-28 11:15:53 +0700

    init

--- js/header.js
+++ js/header.js
@@ -3,9 +3,26 @@ d3.csv("/test/data/header.csv", function(error, data) {
     count = 0;
    
     data.forEach(function(d) {
-        
         var dselect = d3.select("head");
-        dselect.append("meta").attr("name", d.name).attr("content", d.content);
+        
+        //Title
+        if(d.id == "title") dselect.append("title").text(d.title);
+        
+        //Charset
+        if(d.id == "charset") dselect.append("meta").attr("charset", d.charset);
+        
+        //Meta
+        if(d.id == "meta") dselect.append("meta").attr("name", d.name).attr("content", d.content);
+        
+        //Icon
+        if(d.id == "icon") dselect.append("link").attr("rel", d.rel).attr("type", d.type).attr("href", d.href);
+        
+        //Link
+        if(d.id == "css") dselect.append("link").attr("rel", d.rel).attr("type", d.type).attr("href", d.href);
+        
+        //JS
+        if(d.id == "js") dselect.append("script").attr("src", d.href);
+        
     });
 })
 
