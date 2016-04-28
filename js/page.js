0ca7c59f21b92def780b7ef1ecd8cd53b6942e04
Marcus Tran <marcus@hoiio.com>
2016-04-29 00:10:27 +0700

    init

--- js/page.js
+++ js/page.js
@@ -1,3 +1,24 @@
+function toText(col){
+    var colS = "";
+    if (col == 1) colS = "one";
+    else if(cod == 2) colS ="two";
+    else if(cod == 3) colS ="three";
+    else if(cod == 4) colS ="four";
+    else if(cod == 5) colS ="five";
+    else if(cod == 6) colS ="six";
+    else if(cod == 7) colS ="seven";
+    else if(cod == 8) colS ="eight";
+    else if(cod == 9) colS ="nine";        
+    else if(cod == 10) colS ="ten";
+    else if(cod == 11) colS ="eleven";
+    else if(cod == 12) colS ="twelve";
+    else if(cod == 13) colS ="thirteen";
+    else if(cod == 14) colS ="fourteen";
+    else if(cod == 15) colS ="fifteen";
+    else if(cod == 16) colS ="sixteen";
+    return colS;
+}
+
 d3.csv("/test/data/grid.csv", function(error, data) {
     var count;
     count = 0;
@@ -50,7 +71,8 @@ d3.csv("/test/data/grid.csv", function(error, data) {
                 
             //Grid
             if(d.columnOfRow && d.content == "none"){
-                var grid = block.append("div").attr("class","ui " + d.optionGrid + " grid");
+                var column = toText(d.columnOfRow);
+                var grid = block.append("div").attr("class","ui " + column + " column " + d.optionGrid + " grid");
                 
                 for(i=0; i<parseInt(d.row); i++){
                     var row = grid.append("div").attr("class","row");
