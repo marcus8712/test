cf5f08d9e31681f42779af238e6c520d90f50762
Marcus Tran <marcus@hoiio.com>
2016-04-29 01:01:55 +0700

    init

--- js/page.js
+++ js/page.js
@@ -92,7 +92,7 @@ d3.csv("/test/data/grid.csv", function(error, data) {
                 var modalContent = modal.append("div").attr("class","content");
                 modalContent.append("h6").text(d.title);
                 
-                modalContent.append("pre").html(d.modal);
+                modalContent.append("pre").text(d.modal);
                 
                 block.append("script").html("$('#" + d.shortcut + "-modal').modal('attach events', '#" + d.shortcut + " button');");
             }
