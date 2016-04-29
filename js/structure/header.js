9188bcea682960457c5d1a1d41e79d51b30001f5
Marcus Tran <marcus@hoiio.com>
2016-04-29 15:35:58 +0700

    init

--- js/structure/header.js
+++ js/structure/header.js
@@ -1,8 +1,11 @@
 d3.csv("/test/data/header.csv", function(error, data) {
     
     var page = d3.select("head").attr("id");
-        var parent = page.split("-")[0];
-        var child = page.split("-")[1];
+        var str = page.split("-");
+        if(str){
+            var parent = str[0];
+            var child = str[1];
+        }
 
     var header = d3.select("#header");
     
@@ -29,15 +32,21 @@ d3.csv("/test/data/header.csv", function(error, data) {
             if(d.id == "wave") pageInfo.append("img").attr("src", "../img/" + d.href);
         }
         
+        
         //Left Menu
         if(d.id == "left"){
             var itemLeft = leftMenu.append("li").attr("class","item").attr("data-link", d.shortcut);
             
             if(d.shortcut == "intro") {
-                var linkLeft = itemLeft.append("a").attr("href", "../");  
+                var linkLeft = itemLeft.append("a").attr("href", "../");
+                linkLeft.attr("class","active");
             }
             else { 
                 var linkLeft = itemLeft.append("a").attr("href", "../" + d.shortcut + "/" + d.href);
+                
+                if(d.shortcut == parent){
+                    linkLeft.attr("class","active");
+                }
             }
             
             linkLeft.text(d.content);
@@ -48,6 +57,10 @@ d3.csv("/test/data/header.csv", function(error, data) {
             var itemRight = rightMenu.append("li").attr("class","item").attr("data-link", d.shortcut);
             
             var linkRight = itemRight.append("a").attr("href", "../" + d.shortcut + "/" + d.href);    
+            
+            if(d.shortcut == parent){
+                linkRight.attr("class","active");
+            }
             
             linkRight.text(d.content);
         }
