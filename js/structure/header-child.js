eebfe4c0065ebc31ee906745654ec1589981d533
Marcus Tran <marcus@hoiio.com>
2016-04-29 17:31:16 +0700

    init

--- js/structure/header-child.js
+++ js/structure/header-child.js
@@ -1,5 +1,12 @@
 d3.csv("/test/data/header.csv", function(error, data) {
     
+    var page = d3.select("head").attr("id");
+        var str = page.split("-");
+        if(str){
+            var parent = str[0];
+            var child = str[1];
+        }
+
     var header = d3.select("#header");
     
     var navbar = header.append("div").attr("class","navbar");
@@ -16,8 +23,9 @@ d3.csv("/test/data/header.csv", function(error, data) {
         
         
         //Logo
-        if(d.id == "logo") logo.append("img").attr("src", "img/" + d.href); 
-        if(d.id == "wave") pageInfo.append("img").attr("src", "img/" + d.href);
+        if(d.id == "logo") logo.append("img").attr("src", "../img/" + d.href); 
+        if(d.id == "wave") pageInfo.append("img").attr("src", "../img/" + d.href);
+        
         
         
         //Left Menu
@@ -25,13 +33,17 @@ d3.csv("/test/data/header.csv", function(error, data) {
             var itemLeft = leftMenu.append("li").attr("class","item").attr("data-link", d.shortcut);
             
             if(d.shortcut == "intro") {
-                var linkLeft = itemLeft.append("a").attr("class","active");
+                var linkLeft = itemLeft.append("a").attr("href", "../");
             }
             else { 
-                var linkLeft = itemLeft.append("a").attr("href", d.shortcut + "/" + d.href);
+                var linkLeft = itemLeft.append("a").attr("href", "../" + d.shortcut + "/" + d.href);
                 
             }
             
+            if(d.shortcut == parent){
+                linkLeft.attr("class","active");
+                
+            }
             
             linkLeft.text(d.content);
         }
@@ -40,10 +52,26 @@ d3.csv("/test/data/header.csv", function(error, data) {
         if(d.id == "right"){
             var itemRight = rightMenu.append("li").attr("class","item").attr("data-link", d.shortcut);
             
-            var linkRight = itemRight.append("a").attr("href", d.shortcut + "/" + d.href);    
+            var linkRight = itemRight.append("a").attr("href", "../" + d.shortcut + "/" + d.href);    
+            
+            if(d.shortcut == parent){
+                linkRight.attr("class","active");
+            }
             
             linkRight.text(d.content);
         }
+        
+        
+        //Page Info
+        if(d.id == "page"){
+            if(d.shortcut == parent){
+                var itemPage = pageHeader.append("a").attr("href", d.href);
+                itemPage.text(d.content);
+                
+                if(d.href == child) itemPage.attr("class","active");
+            }
+        }
+        
         
 
     });
