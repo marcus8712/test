19e1e838de6005dadb9c622603a011469287a1be
Marcus Tran <marcus@hoiio.com>
2016-05-05 10:51:52 +0700

    update

--- js/component.js
+++ js/component.js
@@ -40,6 +40,42 @@ $(document).ready(function() {
     $(".loading").append(loading);
     
     
+    var navbar ="";
+    navbar +=" <div class=\"navbar\">";
+    navbar +="     <div class=\"logo\">";
+    navbar +="         <img src=\"../img/connected.svg\">";
+    navbar +="     </div>";
+    navbar +="     <div class=\"navbar-content\">";
+    navbar +="         <ul class=\"menu leftMenu\">";
+    navbar +="             <li class=\"item\" data-link=\"intro\"><a href=\"../\">intro</a></li>";
+    navbar +="             <li class=\"item\" data-link=\"structure\"><a class=\"active\">structure</a></li>";
+    navbar +="             <li class=\"item\" data-link=\"style\"><a href=\"../style/font\">styles</a></li>";
+    navbar +="             <li class=\"item\" data-link=\"component\"><a href=\"../component/\">components</a></li>";
+    navbar +="         </ul>
+    navbar +="         <ul class=\"menu rightMenu\">";
+    navbar +="             <li class=\"item\" data-link=\"state\"><a href=\"../state/\">states</a></li>";
+    navbar +="             <li class=\"item\" data-link=\"pattern\"><a href=\"../pattern/\">app patterns</a></li>";
+    navbar +="             <li class=\"item\" data-link=\"sample\"><a href=\"../sample/\">samples</a></li>";
+    navbar +="             <li class=\"item\" data-link=\"download\"><a href=\"../download/\">download</a></li>";
+    navbar +="         </ul>";
+    navbar +="         <ul class=\"menu mobileMenu\">";
+    navbar +="         </ul>";
+    navbar +="     </div>";
+    navbar +=" </div>";
+    $("#header").append(navbar);
+    
+    var pageInfo ="";
+    pageInfo += "<div class=\"page-info\">";
+    pageInfo += "   <div class=\"ui header\">";
+    pageInfo += "       <a class=\"active\">#grids</a>";
+    pageInfo += "       <a href=\"layout\">#layouts</a>";
+    pageInfo += "   </div>";
+    pageInfo += "   <img src=\"../img/wave.svg\">";
+    pageInfo += "</div>";
+    $("#header").append(pageInfo);
+                
+    
+    
     $('#clearing-rows-modal').modal('attach events', '#clearing-rows button');
     $('#specifying-row-width-modal').modal('attach events', '#specifying-row-width button');
     $('#specifying-column-width-modal').modal('attach events', '#specifying-column-width button');
